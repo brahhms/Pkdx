def3db2cab9d3a610e81a64ef75912057ae4fff3
brahhms <abrahamatteo@gmail.com>
2017-11-27 01:15:33 -0600

    se agrega boton favoritos(aun sin funcion)

--- src/main/resources/static/src/ajax.js
+++ src/main/resources/static/src/ajax.js
@@ -30,3 +30,19 @@ function logueame() {
       xhttp.send("username="+p+"&password="+u);
 	
 };
+
+function agregar() {
+	  var s = document.getElementById('s').value;
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {  
+    	//
+      }
+    };
+    xhttp.open("GET", "/", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("favorito="+s);
+	
+};
+
