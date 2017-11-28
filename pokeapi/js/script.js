fd9b203740ae575fa94e67b1a89b939d9912c67c
Victoria Castro <victoria.akicastro92@gmail.com>
2017-11-28 03:57:53 -0600

    se mejoro la salida de datos, quitandole comillas y mstrando evolucion con su nombre ya se le puede dar click a la imagen para redirigir

--- pokeapi/js/script.js
+++ pokeapi/js/script.js
@@ -10,7 +10,7 @@ function buscarPokemon(id) {
   document.getElementById("cargando").src="../img/cargando.gif";//poner a cargar el video o gif
  limpiarDiv();
 	//limpiar si hay imagenes de la busqueda anterior
- 
+
 	document.getElementById("nombre").innerHTML="cargando...";
 
   var cadenaevolutiva = ""; //aqui recolectaremos la cadena evolutiva del puchamon
@@ -21,7 +21,7 @@ function buscarPokemon(id) {
    .then( function(datos) {
 
    		console.log(datos);
-   		//antes de sacar el sprite del json solo usabamos el link 
+   		//antes de sacar el sprite del json solo usabamos el link
    		//var src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+id+".png";
    		var salida ="";
       var salida2="";//la cadena que imprimiremos más tarde
@@ -34,53 +34,60 @@ function buscarPokemon(id) {
 
   	 	//vamos a concatenar la salida
 
-
+      nombre = nombre.slice(1, -1);
  	 	salida= "Nombre: "+nombre;
-    salida2 ="Tipo: "+tipo1; 
-	 		 	
-  	 	
+    tipo1=tipo1.slice(1,-1);
+    salida2 ="Tipo: "+tipo1;
+
+
   	 			try {//esto para los pokes que tienen mas de un tipo
   	 				var tipo2 = JSON.stringify(datos.types[1].type.name);
+            tipo2=tipo2.slice(1,-1);
+
     				salida2 ="Tipos: "+tipo1+" , "+tipo2;
 						}
 				catch(err) {
 					console.log(err);//error que no existe la crap
-          document.getElementById("tipos").innerHTML="Tipos "+tipo1; 
+          document.getElementById("tipos").innerHTML=salida2;
 							}
 
 				try {
   	 				var habilidad = JSON.stringify(datos.abilities[0].ability.name);
+            habilidad=habilidad.slice(1,-1);
   	 				var habilidad2 = JSON.stringify(datos.abilities[1].ability.name);//así obtenemos info del obj que llamamos de la api si existe
+            habilidad2=habilidad2.slice(1,-1);
   	 				salida3 ="Habilidades : "+habilidad+" , "+habilidad2;
 						}
 				catch(err) {
 					var habilidad = JSON.stringify(datos.abilities[0].ability.name);
+          habilidad=habilidad.slice(1,-1);
 					salida3 ="Habilidad : "+habilidad;
 					console.log(err);//error que no existe la crap
-					
+
 							}
 
-			 	 	
+
   	 	document.getElementById("nombre").innerHTML=salida;
       document.getElementById("tipos").innerHTML=salida2;
       document.getElementById("habilidades").innerHTML=salida3;
-  	 
+
    }).catch(function(error){
    	console.log("hubo un error",error);
    	document.getElementById("nombre").innerHTML="se produjo un error (cod 1)"
    });//en el caso que algo salga mal siempre se va a venir aquí
-	
+
 //aquí consultamos el hábitat del puchamon -----------------------------------------------------------------------------------------------------------------------------
 
 	fetch('https://pokeapi.co/api/v2/pokemon-species/'+id)//buscamos la descripción y el hábitat del puchamón
    .then( function (r) { return r.json(); })
-   .then( function(datos) { 		
+   .then( function(datos) {
    		var salida ="";
 
 
    		try {
   	 				var habitat = JSON.stringify(datos.habitat.name);//así obtenemos info del obj que llamamos de la api si existe
-  	 				salida ="Habitat : "+habitat;
+            habitat=habitat.slice(1,-1);
+            salida ="Habitat : "+habitat;
 						}
 				catch(err) {
           var nombre=document.getElementById("nombre").innerHTML;
@@ -89,9 +96,9 @@ function buscarPokemon(id) {
           salida= habitat;
 
           };
-					
+
 					console.log(err);//error que no existe la crap
-					
+
 							}
 
 	 	//mostrar el habitat del pokemondongo
@@ -107,16 +114,16 @@ function buscarPokemon(id) {
         catch(err) {
           cadenaevolutiva="no hay cadena evolutiva";
           console.log(err);
-          
+
               }
 
 
-  	 	
+
   	 	//var descripcion = JSON.stringify(datos.flavor_text_entries[3].flavor_text)
   	 //var descripcion = descripcion.slice(1, -1);//si queremos mostrar la descripcción se haria de esta manera :'v
   	 //document.getElementById("descripcion").innerHTML=descripcion;
- 	 	
-	 		 	
+
+
    }).catch(function(error){
    	console.log("hubo un error",error);
    	document.getElementById("nombre").innerHTML="se produjo un error cod(2)"
@@ -136,23 +143,26 @@ fetch(enlace)
    .then( function(datos) {
 
       var salida ="Cadena evolutiva: ";
-      
 
 
-          salida+= JSON.stringify(datos.chain.species.name); //asi obtenemos el primer pokemon de la cadena evolutiva
-          imprimirImagen(JSON.stringify(datos.chain.species.url));
-         
+          var nombre="";
+          nombre= JSON.stringify(datos.chain.species.name);
+          nombre=nombre.slice(1,-1);
+          //salida+= JSON.stringify(datos.chain.species.name); //asi obtenemos el primer pokemon de la cadena evolutiva
+          imprimirImagen(JSON.stringify(datos.chain.species.url),nombre);
 
 
-           
+
+
           try {
 
                //esto solo para eevee pasaria de la primer vuelta
                try{ for (var i = 0; i <= 7; i++) {
                   var cadena = JSON.stringify(datos.chain.evolves_to[i].species.name);//esta cadena esta para romper el bucle antes de poner la coma
-                  imprimirImagen(JSON.stringify(datos.chain.evolves_to[i].species.url));
-                   salida+=", ";
-                    salida+=cadena;
+                  cadena=cadena.slice(1,-1);
+                  imprimirImagen(JSON.stringify(datos.chain.evolves_to[i].species.url),cadena);
+                   //salida+=", ";
+                    //salida+=cadena;
               };
             }catch(err){
              }
@@ -165,14 +175,16 @@ fetch(enlace)
               }
 
                try{//si tiene una tercera evolucion
-                salida+=", ";
-                salida+=JSON.stringify(datos.chain.evolves_to[0].evolves_to[0].species.name);
-                imprimirImagen(JSON.stringify(datos.chain.evolves_to[0].evolves_to[0].species.url));
+                //salida+=", ";
+                var cadena=JSON.stringify(datos.chain.evolves_to[0].evolves_to[0].species.name);
+               cadena=cadena.slice(1,-1);
+                imprimirImagen(JSON.stringify(datos.chain.evolves_to[0].evolves_to[0].species.url),cadena);
                 try{//si tiene una evolucion alternativa
                   var cadena= datos.chain.evolves_to[0].evolves_to[1].species.name;
-                  salida+=", ";
-                salida+=cadena;
-                imprimirImagen(JSON.stringify(datos.chain.evolves_to[0].evolves_to[1].species.url));
+                  cadena=cadena.slice(1,-1);
+                  //salida+=", ";
+                //salida+=cadena;
+                imprimirImagen(JSON.stringify(datos.chain.evolves_to[0].evolves_to[1].species.url),cadena);
 
 
             }catch(err){
@@ -190,7 +202,7 @@ fetch(enlace)
 
 
 
-          
+
       document.getElementById("cadenaEvolutiva").innerHTML=salida;
       //quitar video
       document.getElementById("cargando").setAttribute( "src", "" );
@@ -205,18 +217,25 @@ fetch(enlace)
 }
 
 
-function imprimirImagen (cod) {
+function imprimirImagen (cod,pokemon) {
 
 
-//obtenemos el id de los pokemons de linea evolutiva 
+//obtenemos el id de los pokemons de linea evolutiva
   var texto= cod.slice(43, -2);//hacemos trim para no hacer otra consulta y mas lenta la app >:v
   var cadena= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+texto+".png"
+  var span = document.createElement("div");
   var nodo = document.createElement("IMG");
+  var nombre = document.createElement("span");//aqui metemos el nombre
+
   nodo.src=cadena;
 nodo.setAttribute( "onClick", "buscarPokemon("+texto+");" );
 //agregar evento onclic al nodo
 
-  document.getElementById("cadenaImg").appendChild(nodo);
+nombre.innerHTML=pokemon+"<br>";
+span.appendChild(nombre);
+span.appendChild(nodo);
+
+  document.getElementById("cadenaImg").appendChild(span);
 
 
 }
@@ -234,7 +253,7 @@ function limpiarDiv (){
 try{
 
 var node=document.getElementById("cadenaImg");
-  
+
 
 while (node.hasChildNodes()) {
     node.removeChild(node.lastChild);
@@ -261,7 +280,3 @@ if(code == 13) { //si presionan enter
 
 
 }
-
-
-
-
